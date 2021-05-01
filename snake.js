6baaba9f608bf030f06212a8cfe6d73791b3a694
Mari Dudek <maridudekj@gmail.com>
2021-05-01 11:45:34 -0600

    onload init()

--- snake.js
+++ snake.js
@@ -4,12 +4,6 @@ const RIGHT_KEY = 39;
 const UP_KEY = 38;
 const DOWN_KEY = 40;
 
-const gameBoard = document.getElementById("gameBoard");
-console.log("width * height =  (after adding element");
-console.log(gameBoard.width + gameBoard.height);
-console.log("width: " + gameBoard.width);
-console.log("height: " + gameBoard.height);
-
 const DOT_SIZE = 10;
 const ALL_DOTS = 900;
 const WIDTH = gameBoard.width;
@@ -18,3 +12,12 @@ const HEIGHT = gameBoard.height;
 
 var x = new Array(ALL_DOTS);
 var y = new Array(ALL_DOTS);
+
+function init() {
+    const gameBoard = document.getElementById("gameBoard");
+    var ctx = gameBoard.getContext('2d');
+    console.log("width * height =  (after adding element");
+    console.log(gameBoard.width + gameBoard.height);
+    console.log("width: " + gameBoard.width);
+    console.log("height: " + gameBoard.height);
+}
