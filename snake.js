b5b3e4f3ce5135db07f2ad8c60bdf2eba51bf884
Mari Dudek <maridudekj@gmail.com>
2021-05-01 11:24:41 -0600

    div -> canvas

--- snake.js
+++ snake.js
@@ -4,11 +4,17 @@ const RIGHT_KEY = 39;
 const UP_KEY = 38;
 const DOWN_KEY = 40;
 
-
-
 const gameContainer = document.getElementById("gameContainer");
 console.log("width * height =  (after adding element");
 console.log(gameContainer.width + gameContainer.height);
-
 console.log("width: " + gameContainer.width);
 console.log("height: " + gameContainer.height);
+
+const DOT_SIZE = 10;
+const ALL_DOTS = 900;
+const WIDTH = gameContainer.width;
+const HEIGHT = gameContainer.height;
+// const max_rand & const delay?
+
+var x = new Array(ALL_DOTS);
+var y = new Array(ALL_DOTS);
