99a35a863f29af7bad9c47b76ff2d013e5920863
Mari Dudek <maridudekj@gmail.com>
2021-05-01 11:16:52 -0600

    print statements

--- snake.js
+++ snake.js
@@ -9,3 +9,6 @@ const DOWN_KEY = 40;
 const gameContainer = document.getElementById("gameContainer");
 console.log("width * height =  (after adding element");
 console.log(gameContainer.width + gameContainer.height);
+
+console.log("width: " + gameContainer.width);
+console.log("height: " + gameContainer.height);
